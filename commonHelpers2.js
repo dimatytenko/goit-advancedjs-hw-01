662d5dbbcf89097ee8eb9dd30ca7a423cebf2c96
dimatytenko <81982752+dimatytenko@users.noreply.github.com>
2023-11-05 13:56:22 +0000

    Deploying to gh-pages from @ dimatytenko/goit-advancedjs-hw-01@85260e07295d57529405a50a00808799d96d7aa4 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{r as t}from"./assets/constants-4ca6f893.js";/* empty css                      */import{P as a,l as o}from"./assets/vendor-78be7656.js";import{a as m,t as s}from"./assets/functions-6e86edb9.js";const e=new a(t.playerRef);e.on("play",()=>{const r=m();e.setCurrentTime(r||0)});e.on("timeupdate",o(s,1e3));
+import{r}from"./assets/constants-4ca6f893.js";/* empty css                      */import{P as t,l as a}from"./assets/vendor-78be7656.js";import{a as o,t as m}from"./assets/functions-6e86edb9.js";const e=new t(r.playerRef),s=o();e.setCurrentTime(s||0);e.on("timeupdate",a(m,1e3));
 //# sourceMappingURL=commonHelpers2.js.map
